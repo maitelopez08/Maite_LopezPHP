321325f3b1e8477f00c231ad43cdd08996ff798a
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-07-04 16:38:21 -0300

    Arquivos PHP

---
+++ funcoes.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções String</title>
+</head>
+<body>
+    <?php
+        #index o12345678901234
+        echo $name = "Stefanie Hatcher<br>";
+        echo $length = strlen($name);
+        echo $cmp = strcmp($name, "Brian Le<br>");
+        echo $index = strpos($name, "e");
+        echo $first = substr($name, 9, 5);
+        echo $name = strtoupper($name);
+    ?>
+</body>
+</html>
---
+++ saida.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print "teste<br>";
+        print "Olá, Mundo\n";
+        print "Escape 'chars' são os MESMOS como em C\n";
+        print "Você pode ter quebras de linhas en uma string. ";
+        print 'Uma string pode usar "aspas-duplas". Isso é muito legal!';
+        print 'Ainda pode-se usar aspas simples dessa forma It\'s cool!';
+    ?>
+    <br><br><br>
+    <?php
+        echo "texto<br>";
+        echo "Olá, Mundo\n";
+        echo "Escape 'chars' são os MESMOS como em C\n";
+        echo "Você pode ter quebras de linhas en uma string. ";
+        echo 'Uma string pode usar "aspas-duplas". Isso é muito legal!';
+        echo 'Ainda pode-se usar aspas simples dessa forma It\'s cool!';
+    ?>
+    
+</body>
+</html>

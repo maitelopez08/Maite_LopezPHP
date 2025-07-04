4018c8269203ce7d694224062760122f294e3b74
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-07-04 17:06:47 -0300

    Atividade

---
+++ interpretada.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $cidade = "Joinville";
+        $estado = "SC";
+        $idade = 325;
+        $frase_capital = "A cidade de $cidade é a capital do $estado";
+        $frase_idade = "$cidade tem mais de $idade anos";
+        echo "<h3>$frase_capital </h3>";
+        echo "<h4>$frase_idade </h4>";
+        ?>
+    <?php
+     $age = 325;
+    print "Today is your birthday.\n"; #$ageth not found
+    print "Today is your ($age)th birthday.\n";
+    ?>
+</body>
+</html>
---
+++ string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $age = 16;
+        print "Você tem ". $age . "anos.\n";
+        print "Você tem $age anos.\n"; #Você tem 16 anos
+
+        //Strings dentro de "" são interpretadas
+        //Variaveis que aparecem dentro deles terão seus valores inseridos na sttring
+        print 'Você tem %age anos.\n '; #Você tem %age anos.\n 
+    ?>
+</body>
+</html>

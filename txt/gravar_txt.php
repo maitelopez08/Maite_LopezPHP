0adfa4a8d1a21799b74c3642ca7e774ef76e805b
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-07-18 15:29:24 -0300

    .

---
+++ txt/gravar_txt.php
@@ -0,0 +1,8 @@
+<?php
+    $texto = file_get_contents("texto.txt");
+    echo nl2br($texto)."<hr/>";
+    $texto = $texto." extra";
+    echo nl2br($texto)."<hr/>";
+    file_put_contents("texto.txt", $texto);
+?>
+

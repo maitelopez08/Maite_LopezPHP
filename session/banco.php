96789dda62255fe01a64702a7fce8fa92cd75fad
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-07-21 17:11:41 -0300

    banco

--- session/banco.php
+++ session/banco.php
@@ -1,7 +1,7 @@
 <?php
     $bdServidor = 'localhost';
     $bdUsuario = 'root';
-    $bdSenha = '';
+    $bdSenha = '123';
     $bdBanco = 'maite_lopez';
     $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
         if(mysqli_connect_errno()) {

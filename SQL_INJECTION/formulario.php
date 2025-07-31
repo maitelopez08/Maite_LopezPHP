0a42bcf01d1d1315e0785112029dce5cd5cdcc31
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-07-31 16:55:03 -0300

    Injection

---
+++ SQL_INJECTION/formulario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ataque SQL</h1>
+    <form action="login_inseguro.php" method="POST">
+        <input type="text" name="nome" placeholder="Digite seu nome">
+        <button type="submit">Entrar</button>
+    </form>
+    <br><br><br><br><br><br>
+
+    <h1>Ataque SQL Negado</h1>
+    <form action="login_inseguro.php" method="POST">
+        <input type="text" name="nome" placeholder="Digite seu nome">
+        <button type="submit">Entrar</button>
+    </form>
+
+</body>
+</html>

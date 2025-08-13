6e6aa42e1845386b4a1017b172377bcb1c3e4611
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-08-13 14:45:46 -0300

    Correção

--- txt/explode_implode.php
+++ txt/explode_implode.php
@@ -1,9 +1,12 @@
 <?php
-    $turma = "Turma de Desenvolvimento";
-    echo "Turma: ".$turma,"<br/>";
-    $turma1 = explode(" ", $turma);
-    echo "turma1: "; print_r ($turma1); echo "<br/>";
-    $turma2 = implode("...", $turma1);
-    echo "turma2: " $turma2
+$turma = "Turma de Desenvolvimento";
+echo "Turma: ".$turma,"<br/>";
 
+$turma1 = explode(" ", $turma);
+echo "turma1: "; 
+print_r($turma1); 
+echo "<br/>";
+
+$turma2 = implode("...", $turma1);
+echo "turma2: " . $turma2;
 ?>
